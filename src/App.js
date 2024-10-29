0f9359cd5d20eab8ea2aea16c3b24f1f206c9c25
1999BHARGAVI <anjic522@gmail.com>
2024-10-29 12:27:46 +0530

    np

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Weatherapplication from './weather-project/firstfile';
+
+function App() {
+  return (
+    <div>
+      <Weatherapplication></Weatherapplication>
+
+    </div>
+  );
+}
+
+export default App;
